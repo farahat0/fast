f9abc2ba461dabdf74aaa1e4217a5b193fb77dd0
farahat0 <ahmedmipf@gmail.com>
2026-02-27 17:35:08 +0200

    33

--- app/images.py
+++ app/images.py
@@ -9,8 +9,7 @@ load_dotenv()
 imagekit = ImageKit(
     public_key=os.getenv("IMAGEKIT_PUBLIC_KEY"),
     private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
-    url_endpoint=os.getenv("IMAGEKIT_URL"),
-)
+    url_endpoint=os.getenv("IMAGEKIT_URL"),)
 '''''
 imagekit = ImageKit(
     private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
